bb4464fe4e50719cca9453f3ac5dc9e43bf75051
대민 <qjrrogoa@github.com>
2021-02-24 16:29:17 +0900

    no message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,17 @@
+import java.lang.System;
+import java.util.Date;
+
 public class HelloWorld{
 
 	public static void main(String[] args){
 		System.out.println("Hello World");
-
-
+		System.out.println("한국");
+		System.out.println("소프트웨어");
+		System.out.println("인재개발원");
+		//오늘 날짜 출력하기
+		Date today = new Date();
+		System.out.print("오늘은");
+		System.out.print(today);
 	}
 
 }
---
+++ ParkDaemin.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class ParkDaemin{
+	public static void main(String[] args){
+		//오늘 날짜 출력하기
+		Date today = new Date();
+		System.out.println();
+}
+
+}
