2f41527b7250819f4e0de873941a2cb9e1587039
대민 <qjrrogoa@github.com>
2021-02-24 17:56:26 +0900

    no message

---
+++ DocsComment.java
@@ -0,0 +1,23 @@
+// 클래스에 대한 설명을 적을때 사용한다.
+/** 
+	@author Park Dae Min
+	@version 1.0
+	
+
+	<h2>This Class is not for Run-Time but for Making Document.</h2>
+
+*/
+public class DocsComment
+{
+	/**
+		@param num1 num1 is first number
+		@param num2 num2 is second number
+		@return return plusing num1 and num2	
+	
+	*/
+	public int getTotal(int num1, int num2)
+	{
+		return num1+num2;
+	}
+
+}
--- ParkDaemin.java
+++ ParkDaemin.java
@@ -1,9 +1,13 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 public class ParkDaemin{
 	public static void main(String[] args){
 		//오늘 날짜 출력하기
-		Date today = new Date();
-		System.out.println();
+		Date day = new Date(); 	//java.util.Date 패키지안에 있다.
+		//2021-02-24 16:40:45 형식으로 출력
+		SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+		String today = dateFormat.format(day);	
+		System.out.println(today);
 }
 
 }
