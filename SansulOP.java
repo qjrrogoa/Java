cf0dbaa2dc4f474ba64435decf0e502043b5a382
Daemin Park <79241184+qjrrogoa@users.noreply.github.com>
2021-02-26 18:17:46 +0900

    Add files via upload
    <week1 day3>
    산술 연산자

--- SansulOP.java
+++ SansulOP.java
@@ -3,9 +3,7 @@ package operator02;
 public class SansulOP {
 
 	public static void main(String[] args) {
-		int result = 3*2+5%2-6/3*5;
-		System.out.format("3*2+5%%2-6/3*5의 결과 : %d",result);
-
+	int result = 3*2+5%2-6/3*5;
+	System.out.format("3*2+5%%2-6/3*5의 결과 : %d",result);
 	}
-
 }
