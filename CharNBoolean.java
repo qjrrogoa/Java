23dbec8032f689d1ad4d0847423b2cd50cdaa68c
대민 <qjrrogoa@github.com>
2021-02-26 11:35:45 +0900

    <week1 day3> String, boolean

---
+++ CharNBoolean.java
@@ -0,0 +1,40 @@
+package datatype01;
+
+public class CharNBoolean {
+
+	public static void main(String[] args) {
+	/*
+	char ch1='가';
+	System.out.println("ch1="+ch1);
+	
+	char ch2='A';
+	int num1 = 2;
+	System.out.println(ch2+num1);
+	//char ch3=ch2+num1;
+	char ch3=(char)(ch2+num1);
+	System.out.println(ch3);
+	
+	
+	char ch6 = '가';
+	char ch6_1 = 44032;
+	char ch6_2 = '\uAC00';
+	char ch6_3 0xAC00;
+	
+	System.out.println("ch6="+ch6);
+	System.out.println("ch6_1="+ch6_1);
+	System.out.println("ch6_2="+ch6_2);
+	System.out.println("ch6_3="+ch6_3);
+	
+	
+	//
+	boolean b1 = true;
+	boolean b2 = false;
+	System.out.println(b1+b2);//[x]
+	System.out.prinln(b1>b2);//[x]
+	System.out.println(b1&&b2);
+	
+	boolean b3='가'>30000;
+	System.out.println(b3);
+	*/
+	}
+}
