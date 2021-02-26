0ce9ac66f339a23f761bb72c45e3ee2562a0e04d
Daemin Park <79241184+qjrrogoa@users.noreply.github.com>
2021-02-26 16:41:37 +0900

    Add files via upload
    <week1 day3>
    산술, 비교 연산자

---
+++ BeeKyoOP.java
@@ -0,0 +1,16 @@
+package operator02;
+
+public class BeeKyoOP {
+
+	public static void main(String[] args) {
+	int num1 = 10, num2=20;
+	System.out.println(num1>num2);
+	boolean b = num1==num2;
+	System.out.printf("%d==%d의 결과:%b%n",num1,num2,b);
+	//false
+	b=num1!=num2;
+	
+
+	}
+
+}
---
+++ SansulOP.java
@@ -0,0 +1,11 @@
+package operator02;
+
+public class SansulOP {
+
+	public static void main(String[] args) {
+		int result = 3*2+5%2-6/3*5;
+		System.out.format("3*2+5%%2-6/3*5의 결과 : %d",result);
+
+	}
+
+}
