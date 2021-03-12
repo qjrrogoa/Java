dda374d91b1327cbaa7ac2e1824f7a8b33ca7bb5
Daemin Park <qjrrogoa@github.com>
2021-03-12 16:19:45 +0900

    no message

--- inheritance16/AnimalApp.java
+++ inheritance16/AnimalApp.java
@@ -2,10 +2,14 @@ package inheritance16;
 
 public class AnimalApp {
 
+
 	public static void main(String[] args) {
 		Dog dog = new Dog("포유류",2,"수컷","시바");
 		dog.printDog();
 		dog.bark();
 		
+		//자식이 아닌 다른 클래스에서 부모(Animal) 멤버(year)에 접근하려면 
+		System.out.println("Dog클래스의 year : "+dog.year);
+		System.out.println("Animal의 year : "+dog.getSuperYear());
 	}
 }
--- inheritance16/Dog.java
+++ inheritance16/Dog.java
@@ -9,19 +9,24 @@ public class Dog extends Animal {
 	//[기본 생성자]
 	public Dog() {
 		super(null,0,null);
+		// this(null,0,null,null); //[x] 동시 호출 불가
 	}
 	
 	//[인자 생성자]
 	public Dog(String species, int year, String gender, String dogKind) {
-		this.species = species;
-		this.gender = gender;
+//		this.species = species;
+//		this.gender = gender;
+//		this.dogKind=dogKind;
+//		//this.year=year; //Dog의 year 초기화
+//		super.year=year;
+		
+		super(species,year,gender);
 		this.dogKind=dogKind;
-		this.year=year; //Dog의 year 초기화
 	}
 	
 	//[멤버 메소드]
 	void bark() {
-		System.out.println(year+"살인 "+dogKind+"가 짖다");
+		System.out.println(super.year+"살인 "+dogKind+"가 짖다");
 	}
 	
 	void printDog() {
@@ -30,6 +35,11 @@ public class Dog extends Animal {
 	}
 	
 	static void staticMethod() {
-		
+		//super [x] 인스턴스 메소드에서만 사용 가능
+		//super() [x] 생성자 안에서만 사용 가능
+	}
+	
+	int getSuperYear() {
+		return super.year; //Animal의 year반환
 	}
 }///////class
